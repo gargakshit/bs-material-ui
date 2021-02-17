d45146835ce5c3df0d65befb42d9d16c33eb7596
Akshit Garg <garg.akshit@gmail.com>
2021-02-17 23:48:23 +0530

    Replace ReactDOMRe with ReactDOM

--- tools/binding-generator/src/helpers/generate-class-prop.ts
+++ tools/binding-generator/src/helpers/generate-class-prop.ts
@@ -1,16 +1,22 @@
 import GenerateReasonName from './generate-reason-name';
 
 const GenerateClassProp = (component: ComponentSignature) => {
-    const classes = component.styles.classes;
-    let toModule = '';
-    let toMakeProps = '';
-    let toMake = '';
-    let toWrapJs = '';
-    if (classes.length) {
-        const classesTypeDefs = classes.map(c => `| ${GenerateReasonName(c)}(string)`).join('\n');
-        const toString = classes.map(c => `| ${GenerateReasonName(c)}(_) => "${c}"`).join('\n');
-        const toClassname = classes.map(c => `| ${GenerateReasonName(c)}(className)`).join('\n');
-        toModule = `
+  const classes = component.styles.classes;
+  let toModule = '';
+  let toMakeProps = '';
+  let toMake = '';
+  let toWrapJs = '';
+  if (classes.length) {
+    const classesTypeDefs = classes
+      .map((c) => `| ${GenerateReasonName(c)}(string)`)
+      .join('\n');
+    const toString = classes
+      .map((c) => `| ${GenerateReasonName(c)}(_) => "${c}"`)
+      .join('\n');
+    const toClassname = classes
+      .map((c) => `| ${GenerateReasonName(c)}(className)`)
+      .join('\n');
+    toModule = `
                 module Classes = {
                     type classesType = ${classesTypeDefs};
                     type t = list(classesType);
@@ -29,17 +35,19 @@ const GenerateClassProp = (component: ComponentSignature) => {
                     );
                 };
             `;
-        toMakeProps = '~classes: Js.Dict.t(string)=?,~style: ReactDOMRe.Style.t=?,';
-        toWrapJs = '~classes=?Belt.Option.map(classes, (v) => Classes.to_obj(v)),~style?,';
-        toMake = '~classes: option(Classes.t)=?,~style: option(ReactDOMRe.Style.t)=?,';
-    }
+    toMakeProps = '~classes: Js.Dict.t(string)=?,~style: ReactDOM.Style.t=?,';
+    toWrapJs =
+      '~classes=?Belt.Option.map(classes, (v) => Classes.to_obj(v)),~style?,';
+    toMake =
+      '~classes: option(Classes.t)=?,~style: option(ReactDOM.Style.t)=?,';
+  }
 
-    return {
-        toModule,
-        toMakeProps,
-        toWrapJs,
-        toMake,
-    };
+  return {
+    toModule,
+    toMakeProps,
+    toWrapJs,
+    toMake,
+  };
 };
 
 export default GenerateClassProp;
